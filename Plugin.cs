5ca597bb93b783fbe70955596325f3e44610ba3e
Macha Celeste <shetah1@yahoo.com>
2025-05-01 12:15:20 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace MysteriousBenefactorPlugin;
+
+[BepInPlugin("com.machaceleste.mysteriousbenefactorplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> initIncome;
+
+    private void Awake()
+    {
+        initIncome = Config.Bind("Main", "Initial Income", 0, new ConfigDescription("Sets how much money your mysterious benifactor deposits when you first make a bank account, setting to 0 disables mod", new AcceptableValueRange<int>(0, 30000)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.mysteriousbenefactorplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/BankHelperServerPatch.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using MysteriousBenefactorPlugin;
+using System;
+
+[HarmonyPatch]
+public class BankHelperServerPatch
+{
+    [HarmonyPatch(typeof(BankHelperServer), "RegisterBank")]
+    class RegisterBankPatch
+    {
+        static void Postfix(ref string user, ref string password, ref string ipPublica, ref string numCuenta, ref string __result)
+        {
+            int amount = Plugin.initIncome.Value;
+            if (amount > 0)
+            {
+                BankAccount userBank = Database.Singleton.GetBankAccount(numCuenta, password, out string error);
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Plugin.Logger.LogInfo(error);
+                    return;
+                }
+                System.Random rnd = new System.Random(Guid.NewGuid().GetHashCode());
+                string[] array = new string[]
+                {
+                    "Pay us back later",
+                    "Seed funding",
+                    "For future endeavors",
+                    "Silent investment",
+                    "No strings attached",
+                    "A favor repaid",
+                    "Just a nudge",
+                    "Welcome gift",
+                    "Off the books",
+                    "Consider this a head start",
+                    "Stay sharp",
+                    "Don't ask, just use",
+                    "A necessary push",
+                    "Keep moving forward",
+                    "From a friend",
+                    "No questions, no problems",
+                    "Call it intuition",
+                    "You'll understand later",
+                    "Trust goes both ways",
+                    "Because you’ll need it"
+                };
+                userBank.Ingreso("unknown", array[rnd.Next(array.Length)], amount);
+                Database.Singleton.SyncBankAccount(userBank);
+            }
+        }
+    }
+}
